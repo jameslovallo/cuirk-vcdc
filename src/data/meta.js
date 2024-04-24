946882b5bf597c475b0fa4d2d9ed12341093763b
James Lovallo <james.lovallo@thrivent.com>
2024-04-24 16:54:56 -0400

    remove unnecessary fonts

--- src/data/meta.js
+++ src/data/meta.js
@@ -13,15 +13,5 @@ export default {
 			rel: 'stylesheet',
 			href: '/src/style.css',
 		},
-		{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-		{
-			rel: 'preconnect',
-			href: 'https://fonts.gstatic.com',
-			crossorigin: 'true',
-		},
-		{
-			href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Libre+Baskerville&display=swap',
-			rel: 'stylesheet',
-		},
 	],
 }
