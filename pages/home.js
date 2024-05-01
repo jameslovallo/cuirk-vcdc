9af94efeda6c88277908520a6240304c74453776
James Lovallo <lovallo.james@gmail.com>
2024-05-01 17:45:35 -0400

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,9 @@
 import { loop, md } from 'cuirk'
 import {
 	card,
-	flyer,
 	grid,
 	homeHeader,
 	linkList,
-	sponsorList,
 } from '../src/components/index.js'
 import links from '../src/data/links.js'
 
@@ -46,18 +44,6 @@ const cards = [
 
 export const body = md`
 ${homeHeader({ eyebrow: 'Welcome to', title: 'The Villages' })}
-
-## Keep It In the Family
-
-Our housing program is unique in that, to date, we have helped enroll 28 households in the Villages to get clear title, set up estate planning, and do minor home repairs that enable our clients to keep their homes (literally) within their families. Due to the success of our program, the work we have done is now being replicated across the city by other organizations! You can assist us in helping more of our own community members by buying a ticket to our spring fundraiser, Building the Villages, or by making a donation directly to us. Any amount helps!
-
-In January 2025, we will begin enrolling another cohort of ten households to Keep It In the Family, provided they live in our service area that stretches from Mount Elliott Avenue in the west to St. Jean Street in the east, from Mack Avenue in the north going all the way down to the Detroit River. Together, we can make a difference for a lot of families in our great community!
-
-${flyer()}
-
-${sponsorList()}
-
-<c-youtube vid="pcH2KAH6F-4"></c-youtube>
 
 ## We Are The Villages
 
