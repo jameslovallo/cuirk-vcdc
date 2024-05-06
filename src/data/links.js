5abfb0cc77ff634b3663e817fd3588b67cdceda6
James Lovallo <james.lovallo@thrivent.com>
2024-05-06 15:28:22 -0400

    update volunteer link

--- src/data/links.js
+++ src/data/links.js
@@ -2,5 +2,6 @@ export default {
 	donate: 'https://syndon.us/campaign/the%20villages%20of%20detroit',
 	newsletter:
 		'https://thevillagesofdetroit.us10.list-manage.com/subscribe/post?u=491160fdb6c928b779465812e&id=df5c91e13e',
-	volunteer: 'https://villagesofdetroit.typeform.com/to/xIrcG1I3',
+	volunteer:
+		'https://docs.google.com/forms/d/e/1FAIpQLSdAZ1wxO9_zHcNU1YVS7xjlOoOqiHlB221dYFLn3Z9l0yI1DQ/viewform',
 }
