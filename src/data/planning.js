b4ddcca6f53c1ce65a3a962081d1b3b505b3e27d
James Lovallo <james.lovallo@thrivent.com>
2024-08-02 16:51:21 -0400

    fix links

--- src/data/planning.js
+++ src/data/planning.js
@@ -7,21 +7,21 @@ export default [
 	{
 		icon: 'MapSearch',
 		title: "Detroit Resident's Guide",
-		href: '/src/downloads/planning/our-community/detroit-residents-guide.pdf',
+		href: '/src/downloads/planning/detroit-residents-guide.pdf',
 	},
 	{
 		icon: 'HomeGroup',
 		title: 'Housing Market Study',
-		href: '/src/downloads/planning/our-community/housing-market-study.pdf',
+		href: '/src/downloads/planning/housing-market-study.pdf',
 	},
 	{
 		icon: 'AccountHardHat',
 		title: 'Infrastructure Advocacy Guide',
-		href: '/src/downloads/planning/our-community/resident-guide-to-neighborhood-infrastructure-advocacy.pdf',
+		href: '/src/downloads/planning/resident-guide-to-neighborhood-infrastructure-advocacy.pdf',
 	},
 	{
 		icon: 'TrashCan',
 		title: 'Alley Maintenance Impact Assessment',
-		href: '/src/downloads/planning/our-community/environmental-and-social-impact-assessment-of-alley-maintenance-in-the-city-of-detroit.pdf',
+		href: '/src/downloads/planning/environmental-and-social-impact-assessment-of-alley-maintenance-in-the-city-of-detroit.pdf',
 	},
 ]
