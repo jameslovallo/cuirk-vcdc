86167b325024beb595a3511b7f13ce575bd09e87
James Lovallo <lovallo.james@gmail.com>
2025-01-13 23:37:16 -0500

    update staff

--- src/data/staff.js
+++ src/data/staff.js
@@ -2,22 +2,12 @@ export default [
 	{
 		name: 'Christianne Sims',
 		description:
-			'Christianne Sims, our board president and a Detroit native, has lived in Jos. Berry Subdivision for over forty years.',
+			'Christianne Sims, our Vice President and a Detroit native, has lived in Jos. Berry Subdivision for over forty years.',
 	},
 	{
 		name: 'Edythe Ford',
 		description:
 			'Edythe Ford, our vice president, a Detroit native has lived in Pingree Park for most of her life, with her family arriving in the neighborhood in 1942.',
-	},
-	{
-		name: 'Tonya Woods Brown',
-		description:
-			'Tonya Woods Brown, our treasurer and a Detroit native, has strong ties to the North Village neighborhood dating back to the 1960s.',
-	},
-	{
-		name: 'Candice Bowman',
-		description:
-			'Candice Bowman is a marketer, communicator and catalyst for organizations and brands and served for the previous four years as the National Director of Outreach and Business Development for PR and integrated marketing firm Lambert & Co. focusing on Detroit, New York and key financial markets. In 2019, Candice launched EQUALSIGN, an equity & inclusion consulting firm focused on integrated marketing and brand strategy, strategic audience engagement, culture-driven marketing and communications, diverse board development for public companies and private equity firms, DEI and ESG consulting. As Founder, Candice is responsible for client service initiatives and the long-term vision of the firm, in addition to heading the strategic account teams on several publicly traded, privately held, education and non-profit clients. Candice is a strong advocate for building inclusive communities, systems and business models for organizations and brands with a focus on equity as the bottom-line impact. A native Detroiter, Candice serves on the board of directors for the Detroit Institute of Arts, Founders Junior Council. She holds a bachelor’s degree in political science from Michigan State University and professional certification in Diversity and Inclusion from Cornell University.',
 	},
 	{
 		name: 'Mac Farr',
@@ -35,11 +25,6 @@ export default [
 			"Ashley Davidson is an award winning creative director and filmmaker with a background in urban design. As the founder of Citizen Brand + Content Lab, she leads a team to reveal shared values through stories and design to generate audiences for organizations supporting community sustainability. Ashley is the co-owner and designer of two Detroit-based hubs: Kiesling and Milwaukee Caffè, a restored 120 year old tavern and adjoining walk-up espresso bar in Detroit’s Milwaukee Junction. Since opening in 2018, Kiesling has won the AIA Award for Commercial Design and been voted Detroit's favorite community bar. Ashley also serves on the board of the DIA Friends of the Detroit Film Theatre, and resides in West Village where she restored a home in 2018.",
 	},
 	{
-		name: 'Damon Dickerson',
-		description:
-			'Damon Dickerson, director, an Ypsilanti native, lives in Islandview',
-	},
-	{
 		name: 'Martha Coates',
 		description:
 			'Martha Coates, director, a Detroit native, lives in East Village',
@@ -52,7 +37,7 @@ export default [
 	{
 		name: 'Robert Gregory',
 		description:
-			"Rob Gregory, our Treasurer, is a native of Detroit and resides in the East Village neighborhood, where he renovated his home in 2022. Rob holds a Bachelor’s in Finance from Michigan State University, a Master’s of Business Administration in Finance and International Business from Wayne State University, and is a Certified Treasury Professional (CTP) Charterholder. Rob has worked his entire professional career in the City of Detroit with Rocket Companies. He has occupied diverse roles within Rocket’s Capital Markets and Finance teams, ranging from bond structuring and trading to overseeing the company's secured funding portfolio. Currently, Rob leads the Pricing Strategy function within Rocket’s Capital Markets Group. Rob has deep familial roots within the villages of Detroit as well as the development of downtown Detroit's parks and urban placemaking. Rob also serves on the board of directors at The East Village Association.",
+			"Rob Gregory, our President, is a native of Detroit and resides in the East Village neighborhood, where he renovated his home in 2022. Rob holds a Bachelor’s in Finance from Michigan State University, a Master’s of Business Administration in Finance and International Business from Wayne State University, and is a Certified Treasury Professional (CTP) Charterholder. Rob has worked his entire professional career in the City of Detroit with Rocket Companies. He has occupied diverse roles within Rocket’s Capital Markets and Finance teams, ranging from bond structuring and trading to overseeing the company's secured funding portfolio. Currently, Rob leads the Pricing Strategy function within Rocket’s Capital Markets Group. Rob has deep familial roots within the villages of Detroit as well as the development of downtown Detroit's parks and urban placemaking. Rob also serves on the board of directors at The East Village Association.",
 	},
 	{
 		name: 'Shayna Moorer',
