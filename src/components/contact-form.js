b33d491fd72f8838f309aaaa7b46b1d1fe94aaa2
James Lovallo <lovallo.james@gmail.com>
2025-01-28 12:58:49 -0500

    add icon gap

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,40 +1,41 @@
-import { html, scss } from 'recluse'
-import { button, grid, icon } from './index.js'
+import { html, scss } from "recluse";
+import { button, grid, icon } from "./index.js";
 
 export const contactForm = () => html`
-	<form method="POST" netlify netlify-honeypot="bot-field">
-		<p hidden>
-			<label>
-				Don’t fill this out if you’re human: <input name="bot-field" />
-			</label>
-		</p>
-		${grid(
-			html`
-				<label>
-					<span>Name<sup></sup></span>
-					<input type="text" name="name" required />
-				</label>
-				<label>
-					<span>Phone Number<sup></sup></span>
-					<input type="tel" name="phone" required />
-				</label>
-			`
-		)}
-		<label>
-			<span>Email Address<sup></sup></span>
-			<input type="email" name="email" required />
-		</label>
-		<label>
-			<span>Your Message<sup></sup></span>
-			<textarea name="message" required></textarea>
-		</label>
-		${button({
-			children: icon({ name: 'Send' }) + 'Send',
-			type: 'submit',
-			shape: 'pill',
-		})}
-	</form>
-`
+  <form method="POST" netlify netlify-honeypot="bot-field">
+    <p hidden>
+      <label>
+        Don’t fill this out if you’re human: <input name="bot-field" />
+      </label>
+    </p>
+    ${grid(
+      html`
+        <label>
+          <span>Name<sup></sup></span>
+          <input type="text" name="name" required />
+        </label>
+        <label>
+          <span>Phone Number<sup></sup></span>
+          <input type="tel" name="phone" required />
+        </label>
+      `
+    )}
+    <label>
+      <span>Email Address<sup></sup></span>
+      <input type="email" name="email" required />
+    </label>
+    <label>
+      <span>Your Message<sup></sup></span>
+      <textarea name="message" required></textarea>
+    </label>
+    ${button({
+      children: icon({ name: "Send", size: "1rem" }) + "Send",
+      type: "submit",
+      shape: "pill",
+      size: "large",
+    })}
+  </form>
+`;
 
 contactForm.style = scss`
 	form {
@@ -80,4 +81,4 @@ contactForm.style = scss`
 			}
 		}
 	}
-`
+`;
