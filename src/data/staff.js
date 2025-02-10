c294e9fa5252efa320341d4bf3e7efbb34506f9d
James Lovallo <lovallo.james@gmail.com>
2025-02-10 14:26:32 -0500

    remove shayna

--- src/data/staff.js
+++ src/data/staff.js
@@ -1,50 +1,47 @@
 export default [
-	{
-		name: 'Christianne Sims',
-		description:
-			'Christianne Sims, our Vice President and a Detroit native, has lived in Jos. Berry Subdivision for over forty years.',
-	},
-	{
-		name: 'Edythe Ford',
-		description:
-			'Edythe Ford, our vice president, a Detroit native has lived in Pingree Park for most of her life, with her family arriving in the neighborhood in 1942.',
-	},
-	{
-		name: 'Mac Farr',
-		description:
-			'Mac Farr, Originally from Jackson, Michigan, Mac attended Michigan State University’s James Madison College.  In 2004, after graduating, he moved to New York where he lived for a decade working in the financial services sector.  Moving to Detroit in 2012, he worked in Mike Duggan’s first campaign for mayor, and in the Mayor’s office during the bankruptcy.  He began with the Villages in 2015.  His work has focused on the development of infrastructure, housing stabilization, transportation, policy advocacy and community engagement.  Mac’s achievements include the development of the housing stabilization program Keep It In the Family, raising $1.5 million since getting to the organization and getting City policies in place such as the alley restoration program as well as the installation of speed humps.  Mac lives in Pingree Park and is a member of the Indian Village Tennis Club and Jefferson Avenue Presbyterian Church.',
-	},
-	{
-		name: 'David Preuss',
-		description:
-			'Br. David Preuss was born in Detroit were his family owned a pharmacy. Joining the Capuchin religious community in 1969, he worked in a variety of ministries mainly in the African-American communities of Milwaukee and Detroit. He became pastor of St. Charles Church (from 1987-2008) and moved to St. Bonaventure(2016 to present) to work in the ministries there. He was on the board of Islandview Village Development Corporation that built affordable housing on Townsend and Baldwin streets, as well as several smaller projects.',
-	},
-	{
-		name: 'Ashley Davidson',
-		description:
-			"Ashley Davidson is an award winning creative director and filmmaker with a background in urban design. As the founder of Citizen Brand + Content Lab, she leads a team to reveal shared values through stories and design to generate audiences for organizations supporting community sustainability. Ashley is the co-owner and designer of two Detroit-based hubs: Kiesling and Milwaukee Caffè, a restored 120 year old tavern and adjoining walk-up espresso bar in Detroit’s Milwaukee Junction. Since opening in 2018, Kiesling has won the AIA Award for Commercial Design and been voted Detroit's favorite community bar. Ashley also serves on the board of the DIA Friends of the Detroit Film Theatre, and resides in West Village where she restored a home in 2018.",
-	},
-	{
-		name: 'Martha Coates',
-		description:
-			'Martha Coates, director, a Detroit native, lives in East Village',
-	},
-	{
-		name: 'Ederique Goudia',
-		description:
-			'Ederique Goudia, director, A Louisiana native, Ederique Goudia has been a resident of East Village since 2018 and a Michigan resident, chef, and food activist for over 20 years. Beyond her ability to create delicious and culturally significant food, she is committed to the health and sustainability of our local community and food system, both here in the Villages and beyond. This is evident through her recent work with Detroit Food Academy, a non-profit that teaches youth about culinary and food entrepreneurship, and Make Food Not Waste, a non-profit committed to keeping food out of landfills, slowing climate change, and creating lasting solutions to food waste. Throughout her long standing career in the hospitality industry, she has also been a fierce advocate for child nutrition, food waste, food insecurity, small business support, minimum wage increase and mental health support for hospitality workers. She is also a dedicated board member of Eastside Community Network, Make Food Not Waste, and a member of the Junior League of Detroit, and Slow Foods USA. ',
-	},
-	{
-		name: 'Robert Gregory',
-		description:
-			"Rob Gregory, our President, is a native of Detroit and resides in the East Village neighborhood, where he renovated his home in 2022. Rob holds a Bachelor’s in Finance from Michigan State University, a Master’s of Business Administration in Finance and International Business from Wayne State University, and is a Certified Treasury Professional (CTP) Charterholder. Rob has worked his entire professional career in the City of Detroit with Rocket Companies. He has occupied diverse roles within Rocket’s Capital Markets and Finance teams, ranging from bond structuring and trading to overseeing the company's secured funding portfolio. Currently, Rob leads the Pricing Strategy function within Rocket’s Capital Markets Group. Rob has deep familial roots within the villages of Detroit as well as the development of downtown Detroit's parks and urban placemaking. Rob also serves on the board of directors at The East Village Association.",
-	},
-	{
-		name: 'Shayna Moorer',
-		description: 'Shayna Moorer is a Detroit native born and raised right here in the, spending equal shares of her life on both the east and west side. She was educated at Osborn High School as well as Wayne State. Shayna is a member of Zeta Phi Beta, and is the proud mother to her son Brice, age 11. Shayna has a passion for community, healthcare and development.'
-	},
-    {
-        name: 'Steve Economy',
-        description: 'Steve Economy owned and operated Standard Barber Company, a Detroit barbershop for over a decade. He is also the Senior Director of Member Engagement at The Conference Board Human Capital Center, where he collaborates with business leaders to address human capital challenges. A Detroit resident for 10+ years, Steve lives in the Villages with his wife and two boys, ages 7 and 9. Stephen came on the board of the Villages CDC in January of 2025 and currently serves as our board secretary.',
-    }
-].sort((a, b) => a.name.localeCompare(b.name))
+  {
+    name: "Christianne Sims",
+    description:
+      "Christianne Sims, our Vice President and a Detroit native, has lived in Jos. Berry Subdivision for over forty years.",
+  },
+  {
+    name: "Edythe Ford",
+    description:
+      "Edythe Ford, our vice president, a Detroit native has lived in Pingree Park for most of her life, with her family arriving in the neighborhood in 1942.",
+  },
+  {
+    name: "Mac Farr",
+    description:
+      "Mac Farr, Originally from Jackson, Michigan, Mac attended Michigan State University’s James Madison College.  In 2004, after graduating, he moved to New York where he lived for a decade working in the financial services sector.  Moving to Detroit in 2012, he worked in Mike Duggan’s first campaign for mayor, and in the Mayor’s office during the bankruptcy.  He began with the Villages in 2015.  His work has focused on the development of infrastructure, housing stabilization, transportation, policy advocacy and community engagement.  Mac’s achievements include the development of the housing stabilization program Keep It In the Family, raising $1.5 million since getting to the organization and getting City policies in place such as the alley restoration program as well as the installation of speed humps.  Mac lives in Pingree Park and is a member of the Indian Village Tennis Club and Jefferson Avenue Presbyterian Church.",
+  },
+  {
+    name: "David Preuss",
+    description:
+      "Br. David Preuss was born in Detroit were his family owned a pharmacy. Joining the Capuchin religious community in 1969, he worked in a variety of ministries mainly in the African-American communities of Milwaukee and Detroit. He became pastor of St. Charles Church (from 1987-2008) and moved to St. Bonaventure(2016 to present) to work in the ministries there. He was on the board of Islandview Village Development Corporation that built affordable housing on Townsend and Baldwin streets, as well as several smaller projects.",
+  },
+  {
+    name: "Ashley Davidson",
+    description:
+      "Ashley Davidson is an award winning creative director and filmmaker with a background in urban design. As the founder of Citizen Brand + Content Lab, she leads a team to reveal shared values through stories and design to generate audiences for organizations supporting community sustainability. Ashley is the co-owner and designer of two Detroit-based hubs: Kiesling and Milwaukee Caffè, a restored 120 year old tavern and adjoining walk-up espresso bar in Detroit’s Milwaukee Junction. Since opening in 2018, Kiesling has won the AIA Award for Commercial Design and been voted Detroit's favorite community bar. Ashley also serves on the board of the DIA Friends of the Detroit Film Theatre, and resides in West Village where she restored a home in 2018.",
+  },
+  {
+    name: "Martha Coates",
+    description:
+      "Martha Coates, director, a Detroit native, lives in East Village",
+  },
+  {
+    name: "Ederique Goudia",
+    description:
+      "Ederique Goudia, director, A Louisiana native, Ederique Goudia has been a resident of East Village since 2018 and a Michigan resident, chef, and food activist for over 20 years. Beyond her ability to create delicious and culturally significant food, she is committed to the health and sustainability of our local community and food system, both here in the Villages and beyond. This is evident through her recent work with Detroit Food Academy, a non-profit that teaches youth about culinary and food entrepreneurship, and Make Food Not Waste, a non-profit committed to keeping food out of landfills, slowing climate change, and creating lasting solutions to food waste. Throughout her long standing career in the hospitality industry, she has also been a fierce advocate for child nutrition, food waste, food insecurity, small business support, minimum wage increase and mental health support for hospitality workers. She is also a dedicated board member of Eastside Community Network, Make Food Not Waste, and a member of the Junior League of Detroit, and Slow Foods USA. ",
+  },
+  {
+    name: "Robert Gregory",
+    description:
+      "Rob Gregory, our President, is a native of Detroit and resides in the East Village neighborhood, where he renovated his home in 2022. Rob holds a Bachelor’s in Finance from Michigan State University, a Master’s of Business Administration in Finance and International Business from Wayne State University, and is a Certified Treasury Professional (CTP) Charterholder. Rob has worked his entire professional career in the City of Detroit with Rocket Companies. He has occupied diverse roles within Rocket’s Capital Markets and Finance teams, ranging from bond structuring and trading to overseeing the company's secured funding portfolio. Currently, Rob leads the Pricing Strategy function within Rocket’s Capital Markets Group. Rob has deep familial roots within the villages of Detroit as well as the development of downtown Detroit's parks and urban placemaking. Rob also serves on the board of directors at The East Village Association.",
+  },
+  {
+    name: "Steve Economy",
+    description:
+      "Steve Economy owned and operated Standard Barber Company, a Detroit barbershop for over a decade. He is also the Senior Director of Member Engagement at The Conference Board Human Capital Center, where he collaborates with business leaders to address human capital challenges. A Detroit resident for 10+ years, Steve lives in the Villages with his wife and two boys, ages 7 and 9. Stephen came on the board of the Villages CDC in January of 2025 and currently serves as our board secretary.",
+  },
+].sort((a, b) => a.name.localeCompare(b.name));
